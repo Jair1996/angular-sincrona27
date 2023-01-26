771ab9aca717c6132fcb68e40b5e1feedbda05a2
Jair Deyner <jairdeyner730791@gmail.com>
2023-01-26 13:24:48 -0500

    Test to CaluculatorService

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ComponentsModule
       ],
       declarations: [
         AppComponent
@@ -26,10 +28,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('sincrona27');
   });
 
-  it('should render title', () => {
+/*   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('sincrona27 app is running!');
-  });
+  }); */
 });
--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -13,4 +13,39 @@ describe('CalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('debe retornar 5 si calculate recibe como parametro 2, 3 y "+"', () => {
+    const result = service.calculate(2, 3, '+');
+    const expectedResult = 5;
+
+    expect(expectedResult).toBe(result);
+  });
+
+  it('debe retornar 10 si calculate recibe como parametro 20, 10 y "-"', () => {
+    const result = service.calculate(20, 10, '-');
+    const expectedResult = 10;
+
+    expect(expectedResult).toBe(result);
+  });
+
+  it('debe retornar 45 si calculate recibe como parametro 9, 5 y "*"', () => {
+    const result = service.calculate(9, 5, '*');
+    const expectedResult = 45;
+
+    expect(expectedResult).toBe(result);
+  });
+
+  it('debe retornar 3 si calculate recibe como parametro 9, 3 y "/"', () => {
+    const result = service.calculate(9, 3, '/');
+    const expectedResult = 3;
+
+    expect(expectedResult).toBe(result);
+  });
+
+  it('debe retornar "Indeterminado" si calculate recibe como parametro 9, 0 y "/"', () => {
+    const result = service.calculate(9, 0, '/');
+    const expectedResult = 'Indeterminado';
+
+    expect(expectedResult).toBe(result);
+  });
 });
